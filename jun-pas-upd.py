3bfe02b261e7ef0e504467daec03949760cabf1e
Domminic <domminicmayer@gmail.com>
2024-07-17 10:04:36 -0700

    ms

--- jun-pas-upd.py
+++ jun-pas-upd.py
@@ -1,15 +1,12 @@
 '''
 Created: 6/24
-Last Modified: 6/13/24
+Last Modified: 7/13/24
 Author: Domminic Mayer
 
-Function: Retrieve onepass information and send that information to ansible files for updating juniper root passwords.
+Function: Retrieve onepass information and send that information to ansible files for updating server root passwords.
 
 TODO: 
-encorporate palo alto network systems for pswd rotations
-alerting when something does go wrong -> slack?
-successful running -> logging
-
+encorporate palo alto network systems
 '''
 
 import os
@@ -21,15 +18,19 @@ import ansible_runner
 import json
 from onepassword import Client
 from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 load_dotenv()
 vault_id = os.getenv("VAULT")
 juniper = os.getenv("JUNIPER")
 ansible = os.getenv("ANSIBLE")
 token = os.getenv("OP_ACCOUNT_TOKEN")
+bot_token = os.getenv("BOT_TOKEN")
 
 playbook = "juniper-root-pwd-upd.yml"
 inventory = "test.yml"
+slk_client = WebClient(bot_token)
 
 def run_playbook(extra_vars):
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -71,6 +72,21 @@ def generate(length, use_digits, use_symbols, use_uppercase, use_lowercase):
 
     return password
 
+def message(msg, status):
+    msg = "Status Code From Script: " + msg
+    channel_id = "C077G0V5CRZ"
+
+    try:
+        response = slk_client.files_upload_v2(
+            channel=channel_id,
+            file="log.json",
+            title="Log File",
+            initial_comment=msg,
+        )
+
+    except SlackApiError as e:
+        print(f"Error sending message: {e.response['error']}")
+
 async def juni_update_pass(passw=None):
     # pre-env check
     if not (vault_id and juniper and token and ansible):
@@ -98,13 +114,14 @@ async def juni_update_pass(passw=None):
 
     ar = []
     for event in r.events:
-        ar.append(event)
+        ar.append(event["stdout"][7::])
     with open('log.json', 'w') as f:
         json.dump(ar, f, indent=4)
     # makes a nicer looking response
 
     if not (r.status == "successful"):
-        print("Ansible script failed!")
+        msg = "Ansible Script Failed!: " + r.status
+        message(msg)
 
 if __name__ == "__main__":
     asyncio.run(juni_update_pass())
